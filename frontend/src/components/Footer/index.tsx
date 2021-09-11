d53e4daeacfaefb72e2216f9d570baed0b42ee3d
Mateus Sagas Stahelin <mateus.stahelin@gmail.com>
2021-09-11 10:02:21 -0300

    Alter configs home page

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/mateus-stahelin" target="_blank" rel="noreferrer">Mateus Sagas Stahelin</a></p>
+                <p className="text-light"><a href="https://github.com/mateus-stahelin/projeto-sds4" target="_blank" rel="noreferrer">App</a> desenvolvido por <a href="https://www.linkedin.com/in/mateus-sagas-stahelin-03177275/" target="_blank" rel="noreferrer">Mateus Sagas Stahelin</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br/>
                 Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
@@ -13,3 +13,5 @@ function Footer() {
   }
   
   export default Footer;
+
+  
